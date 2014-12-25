42844bfdb729c5998668f8de9256e4a2f398e19d
anthonyprz <thony_prz@outlook.com>
2014-12-25 11:47:27 +0100

    segundo ejercicio

---
+++ src/Vocales.java
@@ -0,0 +1,15 @@
+
+public class Vocales {
+	public static void main(String[] args){
+	String palabra ="mi nombre es anthony";
+	int contador = 0;
+	
+	
+		for(int x=0;x<palabra.length();x++) {
+			  if ((palabra.charAt(x)=='a') || (palabra.charAt(x)=='e') || (palabra.charAt(x)=='i') || (palabra.charAt(x)=='o') || (palabra.charAt(x)=='u')){
+			    contador++;
+			  }
+			
+		}System.out.println("el texto ---> " + palabra + " <--- contiene " + contador + " vocales");
+	}
+}
